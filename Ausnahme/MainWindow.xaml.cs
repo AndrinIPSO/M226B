fb6d3fa35e7392d5dd1d7ee0c767130baa4ccdd9
Andrin Schellenberg <andrin.schellenberg@student.ipso.ch>
2021-08-24 08:07:49 +0200

    s

---
+++ Ausnahme/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CircleLibrary;
+
+namespace Ausnahme
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            lb_output.Content = "";
+        }
+
+        private void bt_ok_Click(object sender, RoutedEventArgs e)
+        {
+            Circle circle = new Circle();
+            try
+            {
+                circle.Radius = Convert.ToInt32(tb_input.Text);
+                lb_output.Content += $"\nKreis mit Radius von {tb_input.Text}, wurde erstellt";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Fehler");
+            }
+        }
+    }
+}
---
+++ GeometrischeFiguren/GeomFigure.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometrischeFiguren
+{
+    public class GeomFigure
+    {
+        protected double[] _Seiten;
+
+        public double[] Seiten
+        {
+            get { return _Seiten; }
+            set { _Seiten = value; }
+        }
+
+        public GeomFigure(double[] seiten)
+        {
+            _Seiten = seiten;
+        }
+
+        public string Information()
+        {
+            return "Ist eine Figur ";
+        }
+    }
+}
