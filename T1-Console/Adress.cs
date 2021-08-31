df82ac316aa9927f9fc08ed0e187af1f0806e8c5
Andrin Schellenberg <andrin.schellenberg@student.ipso.ch>
2021-08-31 11:04:24 +0200

    test verbesserung

--- T1-Console/Adress.cs
+++ T1-Console/Adress.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace T1_Console
+﻿namespace T1_Console
 {
     /// <summary>
     /// Adressklasse
